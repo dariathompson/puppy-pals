175cad17f3fa254379bdb6f5bacf78a1a529bf27
dariathompson <daria.kambalina@gmail.com>
2020-07-22 18:35:04 +0100

    dogs model

---
+++ models/Dog.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const DogSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  breed: {
+    type: String,
+    required: false
+  },
+  age: {
+    type: String,
+    required: false
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+module.exports = Dog = mongoose.model("dogs", DogSchema);
