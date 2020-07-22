859396d95ffb163ecf352ec2bb286443be5fb3ee
dariathompson <daria.kambalina@gmail.com>
2020-07-22 16:41:58 +0100

    added hard coded data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Dog from './components/Dog';
+import Lonely from './components/Lonely';
+import data from './data.json'
 
 const App = () => {
+  const [dogs, setDogs] = useState(data);
+  const [likedDogs, setLikedDogs] = useState([]);
+  const [superlikedDogs, setSuperlikedDogs] = useState([]);
+  const [dislikedDogs, setDislikedDogs] = useState([]);
+  const activeDog = 0;
+
+  // switch(action) {
+  //   case 'ADD_TO_LIKED_DOG':
+  //     break;
+  //   case 'ADD_TO_DISLIKED_DOG':
+  //     break;
+  //   case 'ADD_TO_SUPERLIKED_DOG':
+
+  //   default:
+  //     return dogs;
+  // }
+
   return(
-    <p>Doggie Dates</p>
-  )
+    <div className="app">
+      <Header />
+
+      {/* {dogs[1] ? (
+        <Dog
+          key={dogs[1].id}
+          dog={dogs[1]}
+          modifySuperficialChoices={modifySuperficialChoices}
+          likedDogs={likedDogs}
+        />
+      ) : (
+        <Lonely
+          activeDogImage={dogs[activeDog].image}
+          likedDogs={likedDogs}
+          superlikedDogs={superlikedDogs}
+        />
+      )} */}
+    </div>
+  );
 };
 
 export default App;
