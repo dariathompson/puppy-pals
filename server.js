ff0f48834ee2b6338cb42344b36edd7ad4e35608
Patrick Oliver <poliver24@hotmail.com>
2020-07-22 18:04:11 +0100

    mongodb connection made, express server running

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const app = express();
+require('dotenv').config();
+
+// Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+// DB Config
+const db = process.env.ATLAS_URI;
+// Connect to MongoDB
+mongoose
+    .connect(
+        db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
