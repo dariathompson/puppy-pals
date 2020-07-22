9042c1e35d96c43bd58add688c7d6dfb0b9822ce
Patrick Oliver <poliver24@hotmail.com>
2020-07-22 18:48:54 +0100

    added register and login routes plus passport authentication

---
+++ routes/api/dogs.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+// Load User model
+const Dog = require("../../models/Dog")
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post("/register", (req, res) => {
+    // Form validation
+    const {
+        errors,
+        isValid
+    } = validateRegisterInput(req.body);
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    Dog.findOne({
+        username: req.body.username
+    }).then(dog => {
+        if (dog) {
+            return res.status(400).json({
+                username: "Username already exists"
+            });
+        } else {
+            const newDog = new Dog({
+                name: req.body.name,
+                username: req.body.username,
+                breed: req.body.breed,
+                age: req.body.age,
+                email: req.body.email,
+                password: req.body.password
+            });
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newDog.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newDog.password = hash;
+                    newDog
+                        .save()
+                        .then(dog => res.json(dog))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
+
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post("/login", (req, res) => {
+    // Form validation
+    const {
+        errors,
+        isValid
+    } = validateLoginInput(req.body);
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    const username = req.body.username;
+    const password = req.body.password;
+    // Find user by username
+    Dog.findOne({
+        username
+    }).then(dog => {
+        // Check if user exists
+        if (!dog) {
+            return res.status(404).json({
+                usernamenotfound: "Username not found"
+            });
+        }
+        // Check password
+        bcrypt.compare(password, dog.password).then(isMatch => {
+            if (isMatch) {
+                // User matched
+                // Create JWT Payload
+                const payload = {
+                    id: dog.id,
+                    name: dog.name
+                };
+                // Sign token
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey, {
+                        expiresIn: 31556926 // 1 year in seconds
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else {
+                return res
+                    .status(400)
+                    .json({
+                        passwordincorrect: "Password incorrect"
+                    });
+            }
+        });
+    });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
+
+const passport = require("passport");
+const dogs = require("./routes/api/dogs");
+
 require('dotenv').config();
 
 // Bodyparser middleware
@@ -23,5 +27,14 @@ mongoose
     )
     .then(() => console.log("MongoDB successfully connected"))
     .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/dogs", dogs);
+
+    
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
